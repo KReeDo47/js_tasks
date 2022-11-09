ce7d55fd514dadda8e2002802ecc84b1acbb602f
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2022-11-09 22:52:15 +0300

    описываем что сделано

--- GenshinWiki/routes/index.js
+++ GenshinWiki/routes/index.js
@@ -5,5 +5,18 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Страница Heydzo */
+router.get('/Heydzo', function(req, res, next) {
+  res.send("<h1>Страница Heydzo</h1>")
+});
+/* Страница NinGuan */
+router.get('/NinGuan', function(req, res, next) {
+  res.send("<h1>Страница NinGuan</h1>")
+});
+/* Страница Reyzor */
+router.get('/Reyzor', function(req, res, next) {
+  res.send("<h1>Страница Reyzor</h1>")
+});
+
 
 module.exports = router;
