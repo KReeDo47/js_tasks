86a43ff3fc17f6498d11969d16330d2f77f56b54
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2022-11-22 16:50:34 +0300

    Описываем что сделано

---
+++ GenshinWiki/createDB.js
@@ -0,0 +1,21 @@
+var MongoClient = require('mongodb').MongoClient
+
+
+const uri = "mongodb://127.0.0.1:27017/"
+const client = new MongoClient(uri)
+async function run() {
+try {
+await client.connect();
+var database = client.db("GenshinWiki");
+database.dropDatabase()
+database = client.db("GenshinWiki");
+const mystic = database.collection("mystic");
+const result = await mystic.insertOne({name:"Heydzo"});
+console.log(`${result} documents were inserted`);
+} finally {
+await client.close();
+}
+}
+run()
+
+
