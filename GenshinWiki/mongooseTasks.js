0b8ecb945415ed32a5f1c6dcddc413543e9a39b1
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2022-11-22 21:53:22 +0300

    Описываем что сделано

--- GenshinWiki/mongooseTasks.js
+++ GenshinWiki/mongooseTasks.js
@@ -1,13 +1,14 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-var Mystic = mongoose.model('Mystic', { name: String })
+var schema = mongoose.Schema({name: String})
+schema.methods.khayaa = function(){
+    console.log(this.get("name") + " сказал khayaa")
+}
+
+var Mystic = mongoose.model('Mystic', schema);
 
 var person = new Mystic({ name: 'Heydzo' })
 person.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('khayaa')
-    }
+    person.khayaa()
 })
