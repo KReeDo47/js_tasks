b476e7573f13d0238c95eedd32aa01d9b8119198
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2022-12-07 19:39:00 +0300

    9_4

--- GenshinWiki/routes/index.js
+++ GenshinWiki/routes/index.js
@@ -6,7 +6,7 @@ var Mystic = require("../models/mystic").Mystic
 router.get('/', function(req, res, next) {
   Mystic.find({},{_id:0, title:1, nick:1},function(err, menu){
     req.session.greeting = "Hi!!!"
-    res.render('index', { title: 'Express', menu:menu });
+    res.render('index', { title: 'Express', menu:menu, counter: req.session.counter });
   
   })
 });
