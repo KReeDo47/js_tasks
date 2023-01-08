ebe35a1878eea2977a10466ca372fad3ad71a24b
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-01-08 16:05:12 +0300

    ЗАПОЛНЕНИЕ МЕНЮ ИЗ БАЗЫ MySQL

--- GenshinWiki/middleware/createMenu.js
+++ GenshinWiki/middleware/createMenu.js
@@ -1,11 +1,18 @@
 var Mystic = require("./../models/mystic").Mystic
+var db = require('../mySQLConnect')
 
 module.exports = function(req,res,next){
     res.locals.nav = []
-    Mystic.find(null,{_id:0,title:1,nick:1},function(err,result){
-        if(err) throw err
+
+    db.query('SELECT title, nick FROM mystics', (err, result) => {
+        if(err)throw err
         res.locals.nav = result
         next()
     })
+//    Mystic.find(null,{_id:0,title:1,nick:1},function(err,result){
+//        if(err) throw err
+//        res.locals.nav = result
+//        next()
+//    })
 
 }
