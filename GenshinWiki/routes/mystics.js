6374d3b5483e461b9edeea3c84bb5ccd94f3d9fa
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-01-08 14:55:30 +0300

    ВЫВОД  ДАННЫХ НА ЭКРАНАХ MySQL

--- GenshinWiki/routes/mystics.js
+++ GenshinWiki/routes/mystics.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var Mystic = require("../models/mystic").Mystic;
-var checkAuth = require("./../middleware/checkAuth.js")
+var db = require('../mySQLConnect'); 
+//  var Mystic = require("../models/mystic").Mystic;
+//  var checkAuth = require("./../middleware/checkAuth.js")
 var async = require("async")
 
 
@@ -12,17 +13,37 @@ router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с mystics')
 });
 
-/* Страница мистиков */
-router.get('/:nick', checkAuth,function(req, res, next) {
-  Mystic.findOne({nick:req.params.nick}, function(err,Mystic){ 
+
+/* Страница мистиков на MySQL */
+router.get('/:nick' ,function(req, res, next) {
+  db.query(`SELECT * FROM mystics WHERE mystics.nick = '${req.params.nick}'`, (err, mystics) => {
+    if(err) {
+      console.log(err);
       if(err) return next(err)
-      if(!Mystic) return next(new Error("Нет такого мистика в этой игре"))
-      res.render('Mystic', {
-          title: Mystic.title,
-          picture: Mystic.avatar,
-          desc: Mystic.desc
+    } else {
+      if(mystics.length == 0) return next(new Error("Нет такого мистика в этой игре"))
+      var mystic = mystics[0];
+      res.render('mystic', {
+        title: mystic.title,
+        picture: mystic.avatar,
+        desc: mystic.about
       })
+    }
   })
 })
+
+
+/* Страница мистиков на mongodb*/
+// router.get('/:nick', checkAuth,function(req, res, next) {
+//  Mystic.findOne({nick:req.params.nick}, function(err,Mystic){ 
+//      if(err) return next(err)
+//      if(!Mystic) return next(new Error("Нет такого мистика в этой игре"))
+//      res.render('Mystic', {
+//          title: Mystic.title,
+//          picture: Mystic.avatar,
+//          desc: Mystic.desc
+//      })
+// })
+// })
 
 module.exports = router;
