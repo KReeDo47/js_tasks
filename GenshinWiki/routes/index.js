9f0658d1440d895341d26aa7185bf5787760f991
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-01-09 14:28:16 +0300

    Создание таблицы user

--- GenshinWiki/routes/index.js
+++ GenshinWiki/routes/index.js
@@ -1,63 +1,47 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../mySQLConnect')
-// var checkAuth = require("./../middleware/checkAuth.js")
-// var Mystic = require("../models/mystic").Mystic
-// var User = require("./../models/user").User
+var express = require('express')
+var router = express.Router()
+var db = require("../mySQLConnect.js");
 
-/* GET home page. */
+
+
+
 router.get('/', function(req, res, next) {
-  db.query(`SELECT title, nick FROM mystics`, (err, menu) => {
-    req.session.greeting = "Hi!!!",
-        res.cookie('greeting', 'Hi!!!').render('index', {
-            title: 'Express',
-            text: 'Проект "Мистики"',
-            menu: menu,
-            counter: req.session.counter
-})
-});
+  res.render('index', { title: 'Проект "Мистики"', counter:req.session.counter  });
 });
 
-/* Страница регистрации */
-router.get('/logreg', function(req, res, next){
-  res.render('logreg',{
-    title: 'Вход', 
-    error: null
-  });
-})
 
-router.post('/logreg', function(req, res, next){
-  var username = req.body.username;
-  var password = req.body.password;
+router.get('/logreg', function(req, res, next) {
+res.render('logreg',{title: 'Вход', error: null});
+});
 
-  User.findOne({username:username},function(err,user){
-    if(err) return next(err)
-    if(user){
-	    if(user.checkPassword(password)){
-        req.session.user = user._id
-        res.redirect('/')
-      } 
-      else { res.render('logreg', {
-        title: 'Вход', 
-        error: 'Пароль не верный'
-        })
-      }
-    } else {
-	    var user = new User({username:username,password:password})
-      user.save(function(err,user){
+
+
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  // console.log("hey")
+  db.query (`SELECT * FROM user WHERE user.username = '${req.body.username}'`, function(err,users){
         if(err) return next(err)
-          req.session.user = user._id
-          res.redirect('/')
-      })
-    }
-  })
+        if(users.length > 0) {
+          var user = users[0];
+          if (password == user.password){
+            req.session.user = user.user_id
+            res.redirect('/')
+          } else {
+            res.render('logreg', {title: 'Вход', error: 'Пароль не верный'})
+          }
+        } else {
+          db.query(`INSERT INTO user (username, password) VALUES ('${username}', '${password}')`, function(err, user){
+            if(err) return next(err)
+            req.session.user = user.user_id
+            res.redirect('/')
+          })
+        }
+})
 });
-
-/* logout */
 router.post('/logout', function(req, res, next) {
   req.session.destroy()
-  res.locals.user = null
+  req.session.user = null
   res.redirect('/')
 });
-
 module.exports = router;
