bf024d2d035b556b052d8567a3c9288387829cac
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-11 20:27:32 +0300

    ex_33

---
+++ scripts/js/task33.js
@@ -0,0 +1,12 @@
+alert('Ваш возраст:' + prompt('Ваш возраст?')); 
+
+let num1 = prompt('Введите первое число');
+let num2 = prompt('Введите второе число');
+alert(Number(num1) + Number(num2)); 
+
+let a = prompt('Сторона квадрата = ');
+alert(a * a);
+
+let num3 = prompt('Введите первое число');
+let num4 = prompt('Введите второе число');
+alert(Number(num3) + Number(num4)); 
