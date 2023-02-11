58f6125b123e7984980993420e47bceb38895019
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-12 00:18:08 +0300

    ex_35

---
+++ scripts/js/task35.js
@@ -0,0 +1,54 @@
+let num1 = 1;
+let num2 = 2;
+console.log('сумма:' + num1 + num2);
+
+let a = 1;
+let b = 2;
+console.log(a + b);
+
+let num = '123';
+let sum = Number(num[0]) + Number([1]) + Number(num[2]);
+console.log(sum);
+
+let num3 = String(123);
+console.log(num3[0]);
+
+let d = 0;
+console.log(++d);
+
+let num4 = String(123);
+console.log(num4.length);
+
+let e = 24 * 60 * 60;
+console.log(e);
+
+let num5 = 123;
+let str = String(num5);
+console.log(str.length);
+
+let num6 = 123;
+let str1 = String(num6);
+console.log(str1[str1.length]);
+
+let num7 = 123;
+let str2 = String(num7);
+console.log(str2.length);
+
+let num8 = 123;
+let str3 = String(num8);
+console.log(str3[str3.length]);
+
+let f = '123';
+let g = '456';
+let s = Number(f) + String(g);
+
+console.log(s);
+
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+
+console.log(aaa + bbb + ccc);
+
+
+
