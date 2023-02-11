43fcd615780bc8e74530ca474351f74ab0be5a13
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-11 19:38:05 +0300

    ex_30

---
+++ scripts/js/task30.js
@@ -0,0 +1,18 @@
+/*let num = 1;
+num = num + 1;
+num = num + 1;
+alert(num);*/
+alert("Выведется 3")
+
+/*let num1 = 1;
+num1 = num1 + 2;
+num1 = num1 + 3;
+alert(num);*/
+alert("Выведется 6")
+
+let num2 = 47;
+num2 += 7;
+num2 -= 18;
+num2 *= 10;
+num2 /= 15;
+alert(num2);
