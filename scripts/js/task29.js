bb28083ff8f72f8995335080f775c69b431a6f4e
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-11 19:21:43 +0300

    ex_29

---
+++ scripts/js/task29.js
@@ -0,0 +1,14 @@
+let test = '12345';
+alert(Number(test[0]) + Number(test[1]) + Number(test[2])
++ Number(test[3]) + Number(test[4]));
+
+let a = String(12345);
+alert(Number(a[0]) + Number(a[1]) + Number(a[2]) + Number(a[3]) + Number(a[4]));
+
+let b = String(12345);
+alert(Number(b[0]) * Number(b[1]) * Number(b[2]) * Number(b[3]) * Number(b[4]));
+
+let c = String(12345);
+alert((c[4]) + (c[3]) + (c[2]) + (c[1]) + (c[0]));
+
+
