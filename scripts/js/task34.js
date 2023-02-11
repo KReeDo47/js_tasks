fd517f6a01a504c44b5c628901f09eb38a936ed8
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-11 20:41:56 +0300

    ex_34

---
+++ scripts/js/task34.js
@@ -0,0 +1,12 @@
+document.write('This is task1');
+
+document.write("<p>This is task1</p>");
+
+let str = 'Very interesting!!!'
+document.write("<p>" + str + "</p>");
+
+document.write('1<br>');
+document.write('2<br>');
+document.write('3<br>');
+document.write('4<br>');
+document.write('5<br>');
