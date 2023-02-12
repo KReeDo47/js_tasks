e9877f4684f2a0e2bca9c8d37b1f4e7912576c09
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-12 11:04:09 +0300

    ex_38

---
+++ scripts/js/task38.js
@@ -0,0 +1,14 @@
+let array = [1, 2, 3];
+alert(array[0]);
+alert(array[1]);
+alert(array[2]);
+console.log(array[0], array[1], array[2]);
+
+let arr = ['a', 'b', 'c'];
+alert(arr[0]);
+alert(arr[1]);
+alert(arr[2]);
+console.log(arr[0], arr[1], arr[2]);
+
+
+
