5fbad6da1f1acdef9bb7266915da0d7fd08f5aec
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-12 11:19:01 +0300

    ex_39

---
+++ scripts/js/task39.js
@@ -0,0 +1,10 @@
+let array = [1, 2, 3];
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+
+let array2 = [1, 2, 3];
+console.log(array2[0] + array2[1] + array2[2]);
+
+let array3 = ['a', 'b', 'c', 'd'];
+console.log(array3[0] + array3[1], '+', array3[2], '+', array3[3]);
