9e26a9bdde41a01beea076ca30120edeff7cf0eb
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-12 11:45:27 +0300

    ex_41

---
+++ scripts/js/task41.js
@@ -0,0 +1,17 @@
+let arr = ['a', 'b', 'c'];
+arr[0] = '1';
+arr[1] = '2';
+arr[2] = '3';
+console.log(arr);
+
+let arr1 = [5, 6, 7];
+arr1[0] += 3;
+arr1[1] += 3;
+arr1[2] += 3;
+console.log(arr1);
+
+let arr2 = [1, 2, 3];
+arr2[0]++;
+++arr2[1];
+arr2[2]++;
+console.log(arr2); 
