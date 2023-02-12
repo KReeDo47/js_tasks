76d8dc7105361003f1d603b8ef9c52b4fe780291
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-12 10:35:17 +0300

    ex_37

---
+++ scripts/js/task37.js
@@ -0,0 +1,26 @@
+let pi = 3.14
+let r = 5;
+let s = pi * r ** 2;
+alert(s);
+
+let a = 3;
+let s1 = a ** 2;
+alert(s1);
+
+let a1 = 4;
+let b1 = 3;
+let s2 = a1 * b1;
+akert(s2);
+
+let a2 = 7;
+let b2 = 4;
+let p = (a2 + b2) * 2;
+alert(p);
+
+let tc = 25;
+let g = tc * 9/5 + 32;
+alert(g);
+
+let tf = 37;
+let h = (tf - 32) * 5/9;
+alert(h);
