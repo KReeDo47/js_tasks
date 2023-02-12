f797a45d5a650ede9ed692875670d24b6d57418b
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-12 14:36:54 +0300

    ex_52

---
+++ scripts/js/task52.js
@@ -0,0 +1,20 @@
+let obj = {
+    '1a': 1, // кавычки обязательны
+    'b2': 2,
+    'c-c': 3, // кавычки обязательны
+    'd 4': 4, // кавычки обязательны
+    'e5': 5
+};
+
+let obj1 = {
+    '1a': 1,
+    'b2': 2,
+    'c-c': 3,
+    'd 4': 4,
+    'e5': 5
+};
+console.log(obj1['1a']);
+console.log(obj1.b2);
+console.log(obj1['c-c']);
+console.log(obj1['d 4']);
+console.log(obj1.e5);
