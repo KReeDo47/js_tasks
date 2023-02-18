60ac73a4f958ec0c6467984d63677e783ecec9bd
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-18 12:27:12 +0300

    ex_73

---
+++ scripts/js/task73.js
@@ -0,0 +1,28 @@
+let test = 11;
+if (test > 10) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+// 2
+let test1 = 9;
+if (test1 < 10) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+// 3
+let test2 = 10;
+if (test2 >= 10) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+// 4
+let test3 = 7;
+if (test3 <= 10) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
