5b040303f97d19c921a94312de439012595a930e
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-26 00:18:08 +0300

    ex_105

---
+++ scripts/js/task105.js
@@ -0,0 +1,24 @@
+// 1
+let string = 'Ali';
+
+   if (string[0] == 'a' || string[0] == 'A') {
+      console.log('yes');
+   } else {
+      console.log('no');
+   }
+// 2
+let string1 = 'linux';
+
+if (string1[string1.length - 1] == 'x' || string1[string1.length - 1] == 'X') {
+      console.log('yes');
+   } else {
+      console.log('no');  
+   }
+// 3
+let string2 = 'amazing!';
+
+   if (string2[0] == 'a' || string2[0] == 'b') {
+      console.log('yes');
+   } else {
+      console.log('no');  
+   }
